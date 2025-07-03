6b024db8836c2901682bc74a14b84c6b83f6b011
tw-moenandiaung <moenandi.aung@thitsaworks.com>
2025-07-03 15:13:49 +0630

    fix flywayMigration

--- implementation/component/mod_infra/src/main/java/com/thitsaworks/operation_portal/component/infra/flyway/FlywayMigration.java
+++ implementation/component/mod_infra/src/main/java/com/thitsaworks/operation_portal/component/infra/flyway/FlywayMigration.java
@@ -1,19 +1,25 @@
 package com.thitsaworks.operation_portal.component.infra.flyway;
 
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FlywayMigration {
 
-    public static void migrate(Settings settings){
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlywayMigration.class);
+
+    public static void migrate(Settings settings) {
 
         Flyway.configure()
-                .dataSource(settings.url(),settings.username(),
-                        settings.password())
-                .locations(settings.locations())
-                .baselineOnMigrate(true)
-                .load()
-                .migrate();
+              .dataSource(settings.url(), settings.username(),
+                          settings.password())
+              .locations(settings.locations())
+              .baselineOnMigrate(true)
+              .load()
+              .migrate();
 
     }
-    public record Settings(String url,String username,String password,String locations){}
+
+    public record Settings(String url, String username, String password, String... locations) { }
+
 }
