15168ee823a5671a096f9b07e5beebc70eb9cd92
nyeinnyeinei <nyein.ei@thitsaworks.com>
2025-07-07 15:18:22 +0900

    remove Hazelcast related classes

--- implementation/component/mod_misc/src/main/java/com/thitsaworks/operation_portal/component.misc/spring/CacheQualifiers.java
+++ implementation/component/mod_misc/src/main/java/com/thitsaworks/operation_portal/component.misc/spring/CacheQualifiers.java
@@ -2,8 +2,6 @@ package com.thitsaworks.operation_portal.component.misc.spring;
 
 public class CacheQualifiers {
 
-    public static final String HAZELCAST = "Hazelcast";
-
     public static final String PROXY = "Proxy";
 
     public static final String REDIS = "Redis";
