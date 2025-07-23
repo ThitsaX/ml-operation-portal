104f64bcc2fb4dbfd3e79fb6ef809755e6c1fc3f
Nyi Nyi Sai <142963425+nyisai02@users.noreply.github.com>
2025-07-23 16:35:48 +0700

    add some test file

---
+++ implementation/core/mod_hub_services/src/test/java/com/thitsaworks/operation_portal/core/hub_services/query/GetFinancialDataQueryIT.java
@@ -0,0 +1,31 @@
+package com.thitsaworks.operation_portal.core.hub_services.query;
+
+import com.thitsaworks.operation_portal.core.hub_services.BaseVaultSetUpTest;
+import com.thitsaworks.operation_portal.core.hub_services.HubServicesConfiguration;
+import com.thitsaworks.operation_portal.core.hub_services.TestSettings;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {HubServicesConfiguration.class, TestSettings.class})
+public class GetFinancialDataQueryIT extends BaseVaultSetUpTest {
+    private static final Logger log = LoggerFactory.getLogger(GetFinancialDataQueryIT.class);
+
+    @Autowired
+    private GetFinancialDataQuery getFinancialDataQuery;
+
+    @Test
+    public void testExecute() {
+        GetFinancialDataQuery.Input input = new GetFinancialDataQuery.Input("fspID123");
+        GetFinancialDataQuery.Output output = getFinancialDataQuery.execute(input);
+
+        log.info("Financial data retrieved successfully: {}", output.getFinancialData());
+    }
+
+}
+
