6a6c2efdb4f7857715eb0db9736f6328c60d3f33
tw-moenandiaung <moenandi.aung@thitsaworks.com>
2025-08-05 15:21:20 +0630

    fix unix_timestamp

--- implementation/component/mod_infra/src/main/resources/all/db/core/V1_20__init_data_user.sql
+++ implementation/component/mod_infra/src/main/resources/all/db/core/V1_20__init_data_user.sql
@@ -1,11 +1,7 @@
 
 -- Assign HUB-Admin role to the user
 INSERT INTO tbl_principal_role (principal_role_id, principal_id, role_id, created_date, updated_date) VALUES (
-    1,
-    1111111111111111,
-    1,
-    UNIX_TIMESTAMP() * 1000,
-    UNIX_TIMESTAMP() * 1000
+    1, 1111111111111111, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()
 ) AS new_user_role
 ON DUPLICATE KEY UPDATE
     role_id = new_user_role.role_id,
