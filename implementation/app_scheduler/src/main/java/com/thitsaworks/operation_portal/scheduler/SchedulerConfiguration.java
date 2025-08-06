60cc9c041b48c06d934f1bdcc7d7ae3066a321c4
tw-moenandiaung <moenandi.aung@thitsaworks.com>
2025-08-06 09:46:12 +0630

    comment app_scheduler

--- implementation/app_scheduler/src/main/java/com/thitsaworks/operation_portal/scheduler/SchedulerConfiguration.java
+++ implementation/app_scheduler/src/main/java/com/thitsaworks/operation_portal/scheduler/SchedulerConfiguration.java
@@ -1,27 +1,27 @@
-package com.thitsaworks.operation_portal.scheduler;
-
-import com.thitsaworks.operation_portal.component.infra.vault.VaultConfiguration;
-import com.thitsaworks.operation_portal.core.hubuser.HubUserConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-
-@ComponentScan("com.thitsaworks.operation_portal.scheduler")
-@Import(value = {
-        VaultConfiguration.class,
-        VaultBasedApplicationSettings.class, HubUserConfiguration.class})
-public class SchedulerConfiguration {
-
-    @Bean
-    public VaultConfiguration.Settings vaultSettings() {
-
-        return VaultConfiguration.Settings.withPropertyOrEnv();
-    }
-
-    @Bean
-    public AnnouncementScheduler savingPlanScheduler() {
-
-        return new AnnouncementScheduler();
-    }
-
-}
+//package com.thitsaworks.operation_portal.scheduler;
+//
+//import com.thitsaworks.operation_portal.component.infra.vault.VaultConfiguration;
+//import com.thitsaworks.operation_portal.core.hubuser.HubUserConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Import;
+//
+//@ComponentScan("com.thitsaworks.operation_portal.scheduler")
+//@Import(value = {
+//        VaultConfiguration.class,
+//        VaultBasedApplicationSettings.class, HubUserConfiguration.class})
+//public class SchedulerConfiguration {
+//
+//    @Bean
+//    public VaultConfiguration.Settings vaultSettings() {
+//
+//        return VaultConfiguration.Settings.withPropertyOrEnv();
+//    }
+//
+//    @Bean
+//    public AnnouncementScheduler savingPlanScheduler() {
+//
+//        return new AnnouncementScheduler();
+//    }
+//
+//}
