8a374cf72514734cb25cd2b8eafe898bf13386fa
tw-moenandiaung <moenandi.aung@thitsaworks.com>
2025-08-21 17:01:13 +0630

    fix GenerateAuditReport interface

--- implementation/uc_operation_portal/src/main/java/com/thitsaworks/operation_portal/usecase/operation_portal/GenerateAuditReport.java
+++ implementation/uc_operation_portal/src/main/java/com/thitsaworks/operation_portal/usecase/operation_portal/GenerateAuditReport.java
@@ -4,13 +4,12 @@ import com.thitsaworks.operation_portal.component.common.identifier.ActionId;
 import com.thitsaworks.operation_portal.component.common.identifier.RealmId;
 import com.thitsaworks.operation_portal.component.common.identifier.UserId;
 import com.thitsaworks.operation_portal.component.misc.exception.DomainException;
+import com.thitsaworks.operation_portal.component.misc.usecase.UseCase;
 import com.thitsaworks.operation_portal.reporting.report.exception.ReportException;
 
 import java.time.Instant;
 
-public interface GenerateAuditReport {
-
-    Output execute(Input input) throws DomainException;
+public interface GenerateAuditReport extends UseCase<GenerateAuditReport.Input, GenerateAuditReport.Output> {
 
     record Input(RealmId realmId,
                  Instant fromDate,
