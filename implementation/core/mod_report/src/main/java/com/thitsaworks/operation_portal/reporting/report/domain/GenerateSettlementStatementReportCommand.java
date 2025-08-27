dbbc7a76ebb7c266599b9fa23be92abf54fb5ddf
nniwin <143474987+nniwin@users.noreply.github.com>
2025-08-27 13:33:05 +0630

    implement Settlement Statement Report

---
+++ implementation/core/mod_report/src/main/java/com/thitsaworks/operation_portal/reporting/report/domain/GenerateSettlementStatementReportCommand.java
@@ -0,0 +1,20 @@
+package com.thitsaworks.operation_portal.reporting.report.domain;
+
+import com.thitsaworks.operation_portal.reporting.report.exception.ReportException;
+
+import java.time.Instant;
+
+public interface GenerateSettlementStatementReportCommand {
+
+    record Input(String fspId,
+                 String startDate,
+                 String endDate,
+                 String filetype,
+                 String currencyId,
+                 String timeZoneOffset) { }
+
+    record Output(byte[] statementRptData) { }
+
+    Output execute(Input input) throws ReportException;
+
+}
