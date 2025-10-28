4ff2c102bb259ea011c32758768e1cc1e85e6615
moenandiaung <moenandi.aung@thitsaworks.com>
2025-10-28 10:20:31 +0630

    added sorting in actionList and participantUserList

--- implementation/web_api_operation/src/main/java/com/thitsaworks/operation_portal/api/operation/portal/controller/coreServices/GetActionListByUserController.java
+++ implementation/web_api_operation/src/main/java/com/thitsaworks/operation_portal/api/operation/portal/controller/coreServices/GetActionListByUserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,11 +30,12 @@ public class GetActionListByUserController {
         var output = this.getActionListByUser.execute(input);
 
         List<Response.Action> actionNames = output.actionNames()
-                      .stream()
-                      .map(actionName -> new Response.Action(String.valueOf(actionName.actionId()
-                                                                                      .getId()),
-                              actionName.actionName()))
-                      .collect(Collectors.toList());
+                                                  .stream()
+                                                  .map(actionName -> new Response.Action(String.valueOf(actionName.actionId()
+                                                                                                                  .getId()),
+                                                                                         actionName.actionName()))
+                                                  .sorted(Comparator.comparing(Response.Action::actionName))
+                                                  .collect(Collectors.toList());
 
         var response = new Response(actionNames);
 
@@ -45,9 +47,8 @@ public class GetActionListByUserController {
 
     public record Response(List<Action> actionList) {
 
-        public record Action(String actionId, String actionName) implements Serializable {
-
-        }
+        public record Action(String actionId,
+                             String actionName) implements Serializable { }
 
     }
 
