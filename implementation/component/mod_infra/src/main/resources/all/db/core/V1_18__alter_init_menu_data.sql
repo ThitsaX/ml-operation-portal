4eff090ad22c29d2205afff05a124622d7b7cf09
moenandiaung <moenandi.aung@thitsaworks.com>
2026-01-13 10:58:28 +0630

    https://thitsa.atlassian.net/browse/MOJ-961
    https://thitsa.atlassian.net/browse/MOJ-961

---
+++ implementation/component/mod_infra/src/main/resources/all/db/core/V1_18__alter_init_menu_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tbl_menu (menu_id,name,parent_id,is_active,created_date,updated_date) VALUES
+     (23,'Transaction Detailed Report','13',1,1768273439,1768273439),
+     (24,'Management Summary Report','13',1,1768273439,1768273439);
