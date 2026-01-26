5b43698a20a9d1b17da8136da4e2774d482f8378
moenandiaung <moenandi.aung@thitsaworks.com>
2026-01-26 12:28:19 +0630

    FIX errors

--- implementation/component/mod_misc/src/main/java/com/thitsaworks/operation_portal/component.misc/MiscConfiguration.java
+++ implementation/component/mod_misc/src/main/java/com/thitsaworks/operation_portal/component.misc/MiscConfiguration.java
@@ -2,6 +2,7 @@ package com.thitsaworks.operation_portal.component.misc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.thitsaworks.operation_portal.component.misc.spring.SpringContext;
 import org.springframework.context.annotation.Bean;
@@ -31,6 +32,7 @@ public class MiscConfiguration {
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new Jdk8Module());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
