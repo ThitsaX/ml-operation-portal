1d40da3e0af22ea0798b5288b062d0bc382a14a7
moenandiaung <moenandi.aung@thitsaworks.com>
2026-03-17 10:57:46 +0630

    Add role and menu (https://thitsa.atlassian.net/browse/GIN-116)

---
+++ implementation/component/mod_infra/src/main/resources/all/db/core/V1_23__insert_role_menu_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tbl_menu (menu_id,name,parent_id,is_active,created_date,updated_date) VALUES
+(25,'DFSP Settlement Overview Report','13',1,1773720045,1773720045);
+
+INSERT INTO tbl_role (role_id,name,active,is_dfsp,created_date,updated_date) VALUES
+(7,'Guinea DFSP Operator',1,1,1773720045,1773720045);
